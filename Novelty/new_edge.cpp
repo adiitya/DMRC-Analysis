d98d8196bd23be1152e0bd13defa84a2aadea604
kapilks <kapilkumar.iitr@gmail.com>
2017-03-17 01:04:21 +0530

    file io added for dij and aij

--- Novelty/new_edge.cpp
+++ Novelty/new_edge.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 
 const int MAX = 154;
-const int NUM_OF_EDGE = 30;
+const int NUM_OF_EDGE = 250;
 const int THRESHOLD_DIST = 10;
 const int THRESHOLD_DIFF = 8;
 
@@ -13,6 +13,7 @@ const int THRESHOLD_DIFF = 8;
 double w[MAX][MAX];
 double dist[MAX][MAX];
 double longdist[MAX][MAX];
+int aij[MAX][MAX];
 
 
 void floyd(double sd[MAX][MAX], int n)
@@ -55,6 +56,10 @@ int main()
 		w[v][u] = d;
 		w[u][u] = 0;
 		w[v][v] = 0;
+		aij[u][v]=1;
+		aij[v][u]=1;
+		aij[u][u]=0;
+		aij[v][v]=0;
 	}
 
 	for(int i = 0; i < N; ++i)
@@ -99,10 +104,32 @@ int main()
 
 		w[s][e] = longdist[s][e];
 		w[e][s] = longdist[s][e];
+		aij[s][e]=1;
+		aij[e][s]=1;
 
 		a++;
 	}
 
-	for(int i = 0; i < newedge.size(); ++i)
-		cout<<newedge[i].first<<" "<<newedge[i].second<<endl;
+	floyd(dist, N);
+	ofstream dij("allPairShortestPathMatrix(dij).txt");
+	ofstream obj("AdjacencyMatrix(aij).txt");
+	dij.precision(3);
+	for(int i=1;i<=N;i++)
+	{
+		for(int j=1;j<=N;j++)
+		{
+			dij<<fixed<<((dist[i][j]>=1e9)?-1:dist[i][j])<<" ";
+			obj<<aij[i][j]<<" ";
+		}
+		dij<<endl;
+		obj<<endl;
+	}
+
+
+
+
+	/*for(int i = 0; i < newedge.size(); ++i)
+		cout<<newedge[i].first<<" "<<newedge[i].second<<endl;*/
+
+
 }
